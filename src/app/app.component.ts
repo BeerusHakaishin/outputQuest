8453edfd40c4ee5457ed1dea9e15da0e78d8aab3
isma <meziani-i@outlook.fr>
2021-11-07 21:38:29 +0100

    settings up quest exigence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'outputQuestSoluce';
+  public onomatopoeias: string[] = [];
+  title = 'outputQuest';
+
+  onReceiveNewOnomatopia(onomatopeia: string) {
+    this.onomatopoeias.push(onomatopeia);
+  }
 }
---
+++ src/app/create-onomatopia/create-onomatopia.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-create-onomatopia',
+  templateUrl: './create-onomatopia.component.html',
+  styleUrls: ['./create-onomatopia.component.css'],
+})
+export class CreateOnomatopiaComponent implements OnInit {
+
+  newOnomatopia: string | undefined;
+
+  @Output() public sendOnomatopiaToParent: EventEmitter<any> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  createOnomatopia(): void {
+    this.sendOnomatopiaToParent.emit(this.newOnomatopia);
+  }
+  
+}
